51c4bc198cf14e4bb356ea8bf688eecf3244e1a4
GeoFox-bingyan <wangkai_geo@163.com>
2019-01-30 12:32:01 +0800

    mygit

--- mygit.sh
+++ mygit.sh
@@ -1,8 +1,8 @@
-git init
+# git init
 
 git add .
 
-git commit -m "Ruby 概述"
+git commit -m "mygit"
 git remote add origin git@github.com:GeoKylin/Step-By-Step-For-RubyOnRails.git
 
 git pull --rebase origin master
